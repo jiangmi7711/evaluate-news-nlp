1ce2978e280b667eaea947250c8b03e7246537fd
jiangmi7711 <scutjiangm@gmail.com>
2021-05-28 22:30:45 -0700

    sass style

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -9,6 +10,14 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader']
             }
         ]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -11,6 +11,14 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader']
             }
         ]
     },
