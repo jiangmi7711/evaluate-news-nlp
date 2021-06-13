6d4fcaecca45779e27fc6c2fae8268172e43d5db
jiangmi7711 <scutjiangm@gmail.com>
2021-06-13 15:40:26 -0700

    validation first

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -38,7 +38,7 @@ const postData = async (url = '', data = {}) => {
     if (res.agreement === undefined && res.confidence === undefined && res.subjectivity === undefined && res.irony === undefined && res.score_tag ===undefined) {
     alert("Please input url!")
     } else {
-    document.getElementById('results').innerHTML = Agreement: ${res.agreement} Subjectivity: ${res.subjectivity} Confidence: ${res.confidence} Irony: ${res.irony} Score_tag: ${res.score_tag};
+    document.getElementById('results').innerHTML = `Agreement: ${res.agreement}    Subjectivity: ${res.subjectivity}    Confidence: ${res.confidence}    Irony: ${res.irony}    Score_tag: ${res.score_tag}`;
     }
     })
     
